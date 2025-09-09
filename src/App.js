39fa48f48a492017d79847f1ce8740a1273fb63a
Kuba Karnath <jacob_jk@wp.pl>
2025-09-09 22:53:41 +0200

    big commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [isExpanded, setIsExpanded] = useState(false); // toggle state
+
+  const myDate = date.toLocaleString("pl-PL", {
+    minute: "numeric",
+    hour: "2-digit",
+  });
+  const mySecondDate = date.toLocaleString("pl-PL", {
+    day: "2-digit",
+    month: "numeric",
+    year: "numeric",
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // function to toggle class
+  const toggleUni = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="gora"></div>
+      <div className="srodek">
+        <div className="projekt">
+          <div className="profil"></div>
+        </div>
+      </div>
+      <div
+        className={isExpanded ? "unirozw" : "uni"}
+        onClick={toggleUni}
+        style={{ cursor: "pointer" }}
+      />
+      <div className="bodydol">
+        <div className="date">
+          {mySecondDate} {""}
+          {myDate}
+        </div>
+      </div>
+      <div className="dol" />
+    </>
   );
 }
 
